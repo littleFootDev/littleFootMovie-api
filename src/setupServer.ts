81d86e3928c38861709d2345c4d676a129d25615
littleFootDev <thelittlefootdev@gmail.com>
2022-06-12 22:09:36 +0200

    adding dotenv en environelent variable

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,6 +1,9 @@
 import Koa from 'koa';
 import koaCors from 'koa2-cors';
 import bodyParser from 'koa-bodyparser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function serverSetup() {
     const server : Koa = new Koa();
@@ -16,7 +19,7 @@ function middleware(server : Koa) {
 
 async function startServer(server : Koa) {
     try {
-        const port = 8000;
+        const port = process.env.PORT;
         const serverStarted: Promise<void> =new Promise(resolve => {
             server.listen(port, resolve);
             console.log(`Server started on port ${port}`);
